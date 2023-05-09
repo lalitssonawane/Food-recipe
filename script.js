924f6b39dad8c6994c903c6c576dfaf3bfcaf3fe
Lalit-7881 <lalitsonawane391@gmail.com>
2023-05-09 23:20:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const menu = document.querySelector(".menu");
+const icons = document.querySelectorAll(".section-1-icons i");
+
+menu.addEventListener("click", () => {
+  document.querySelectorAll(".target").forEach((item) => {
+    item.classList.toggle("change");
+  });
+});
+
+let i = 1;
+
+setInterval(() => {
+  i++;
+
+  const icon = document.querySelector(".section-1-icons .change");
+
+  icon.classList.remove("change");
+
+  if (i > icons.length) {
+    icons[0].classList.add("change");
+    i = 1;
+  } else {
+    icon.nextElementSibling.classList.add("change");
+  }
+}, 4000);
